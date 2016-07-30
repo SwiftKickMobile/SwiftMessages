06be7b8ee328f24b17c439df49e1b5e4ad44f211
Timothy Moose <tim@swiftkick.it>
2016-07-30 10:40:07 -0500

    Initial Commit

---
+++ SwiftMessages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMessages
+//
+//  Created by Tim Moose on 7/30/16.
+//  Copyright © 2016 SwiftKick Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
