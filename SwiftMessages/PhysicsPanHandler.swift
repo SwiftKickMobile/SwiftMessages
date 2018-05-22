4bb93a337e8cdce80e148dcdba9825e29d194721
Timothy Moose <tim@swiftkick.it>
2018-05-22 11:13:45 -0500

    Tiny refactor

--- SwiftMessages/PhysicsPanHandler.swift
+++ SwiftMessages/PhysicsPanHandler.swift
@@ -160,30 +160,28 @@ open class PhysicsPanHandler {
     }
 
     private func configureSafeAreaWorkaround() {
-        guard let messageView = messageView else { return }
-        if #available(iOS 11, *) {
-            // Freeze the layout margins (with respect to safe area) in order to work
-            // around to a visual glitch (bug?) on iOS 11 where the message view's motion
-            // becomes temporarily discontinuous. The problem can be seen in the Demo app's
-            // "Centered" example by panning or flinging the message view diagonally up while
-            // the device in portrait orientation. As the message view enters the top safe area,
-            // the top layout margin gets a proportinal increase, causing the background view's
-            // vertical velocity to abruptly go to zero. Once fully inside the safe area, the
-            // layout margin has reached it's maximum value and the vertical velocity abruptly
-            // resumes. By freezing the layout margins here (the message view's resting layout
-            // would have already been established), we completely avoid the problem. This could
-            // concievably break message views that need to have the layout margins affected by
-            // the save area continuously, but this doesn't seem like a likely scenario. Strangly,
-            // this problem doesn't affect left, right or bottom safe areas or any device
-            // orientation other than portrait.
-            func freezeLayoutMargins(view: UIView) {
-                let margins = view.layoutMargins
-                view.insetsLayoutMarginsFromSafeArea = false
-                view.layoutMargins = margins
-                view.subviews.forEach { freezeLayoutMargins(view: $0) }
-            }
-            freezeLayoutMargins(view: messageView)
+        guard #available(iOS 11, *), let messageView = messageView else { return }
+        // Freeze the layout margins (with respect to safe area) in order to work
+        // around to a visual glitch (bug?) on iOS 11 where the message view's motion
+        // becomes temporarily discontinuous. The problem can be seen in the Demo app's
+        // "Centered" example by panning or flinging the message view diagonally up while
+        // the device in portrait orientation. As the message view enters the top safe area,
+        // the top layout margin gets a proportinal increase, causing the background view's
+        // vertical velocity to abruptly go to zero. Once fully inside the safe area, the
+        // layout margin has reached it's maximum value and the vertical velocity abruptly
+        // resumes. By freezing the layout margins here (the message view's resting layout
+        // would have already been established), we completely avoid the problem. This could
+        // concievably break message views that need to have the layout margins affected by
+        // the save area continuously, but this doesn't seem like a likely scenario. Strangly,
+        // this problem doesn't affect left, right or bottom safe areas or any device
+        // orientation other than portrait.
+        func freezeLayoutMargins(view: UIView) {
+            let margins = view.layoutMargins
+            view.insetsLayoutMarginsFromSafeArea = false
+            view.layoutMargins = margins
+            view.subviews.forEach { freezeLayoutMargins(view: $0) }
         }
+        freezeLayoutMargins(view: messageView)
     }
 }
 
