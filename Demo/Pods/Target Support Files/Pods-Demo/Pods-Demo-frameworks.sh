42c3fbec8bb217bbf0ae35b06980247b16477d60
Timothy Moose <tim@swiftkick.it>
2018-05-25 09:04:42 -0500

    Add iMessageDemo target to Demo project

--- Demo/Pods/Target Support Files/Pods-Demo/Pods-Demo-frameworks.sh
+++ Demo/Pods/Target Support Files/Pods-Demo/Pods-Demo-frameworks.sh
@@ -144,9 +144,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftMessages/SwiftMessages.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftMessages-AppExtension/SwiftMessages.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftMessages/SwiftMessages.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftMessages-AppExtension/SwiftMessages.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
