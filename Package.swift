16c41224c599c062c2baf0c6c2944f635abd1bca
Timothy Moose <tim@swiftkick.it>
2020-08-19 11:52:12 -0500

    Update swift tools version to support resources

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "SwiftMessages",
-    platforms: [.iOS("9.0")],
+    platforms: [
+        .iOS("9.0")
+    ],
     products: [
         .library(name: "SwiftMessages", targets: ["SwiftMessages"])
     ],
